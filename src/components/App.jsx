0e83462160000387c5801b83848103557a9077c4
VozzRoman <looper0500@ukr.net>
2022-12-28 13:35:55 +0200

    adding

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Box } from './Box/Box';
+
+
+
+export class App extends Component {
+
+	state = {
+  		good: 0,
+  		neutral: 0,
+  		bad: 0
+}
+
+	render() {
+		return (
+			<>
+				<Box
+					maxWidth="1140px"
+					m="0 auto"
+					background="orange"
+					p="20px"
+
+				>
+				<section>
+				<h1>Please Leave feedback</h1>
+			
+				<button>Good</button>
+				<button>Neutral</button>
+				<button>Bad</button>		
+				
+				</section>
+				<section>
+					<h2>Statistics</h2>
+						<p>Good:<span>{this.state.good}</span></p>
+						<p>Neutral:<span>{this.state.neutral}</span></p>
+						<p>Bad:<span>{this.state.bad}</span></p>	
+				</section>
+				</Box>
+	
+			
+
+
+			</>
+	)
+}
+}
+
