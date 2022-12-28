23a78787f25d25e3cdd75ff5aee877eefdb17a3f
VozzRoman <looper0500@ukr.net>
2022-12-28 17:04:06 +0200

    adding

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -35,29 +35,41 @@ export class App extends Component {
 		})
 	}
 
+	// countTotalFeedback = () => {
+	// 	const keys = this.state;
+	// 	const total = Object.values(keys)
+	// 	console.log(total);
+	// }
+
+	// countPositiveFeedbackPercentage = () => {
+	// 	console.log('43%')
+	// } 
+
 	render() {
+		// const keys = this.state;
+		// const value = Object.values(keys)
+		// const total = value.reduce((acc, number) => {
+		// 	return acc + number;
+		// }, 0)
+		const total = this.state.good + this.state.neutral + this.state.bad;
 		return (
 			<>
-				<Box
-					maxWidth="1140px"
-					m="0 auto"
-					background="orange"
-					p="20px"
-
-				>
+				<Box maxWidth="1140px" m="0 auto" background="orange" p="20px">
 				<section>
 				<h1>Please Leave feedback</h1>
 			
 				<button onClick={this.onClickGood}>Good</button>
 				<button onClick={this.onClickNeutral}>Neutral</button>
-				<button onClick={this.onClickBad}>Bad</button>		
+				<button onClick={this.onClickBad}>Bad</button>			
 				
 				</section>
 				<section>
 					<h2>Statistics</h2>
 						<p>Good:<span>{this.state.good}</span></p>
 						<p>Neutral:<span>{this.state.neutral}</span></p>
-						<p>Bad:<span>{this.state.bad}</span></p>	
+						<p>Bad:<span>{this.state.bad}</span></p>
+						<p>Total:<span>{total}</span></p>
+						{/* <p>Positive feedback:<span>{this.countTotalFeedback()}</span></p> */}
 				</section>
 				</Box>
 	
@@ -69,3 +81,4 @@ export class App extends Component {
 }
 }
 
+	
